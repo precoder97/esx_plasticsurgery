46ef06d511b1f8c90ec07f9926a9830e4f3e2eda
Dom GiGi <38845277+DomGiGi@users.noreply.github.com>
2018-11-15 04:47:12 +0000

    Update en.lua

--- esx_plasticsurgery/locales/en.lua
+++ esx_plasticsurgery/locales/en.lua
@@ -1,10 +1,10 @@
-Locales['en'] = {
+Locales['br'] = {
 
     ['valid_purchase'] = 'Validar Esta Compra?',
     ['yes'] = 'Sim',
     ['no'] = 'Nao',
     ['not_enough_money'] = 'Nao Tem Dinheiro Sufeciente',
     ['press_access'] = 'Pressiona ~INPUT_CONTEXT~ Para Aceder Ao Menu',
-    ['you_paid'] = 'you paid ',
-    ['Cirugiao'] = 'Lux Cirugias Plasticas',
+    ['you_paid'] = 'Voce Pagou ',
+    ['Cirugiao'] = 'Cirugias Plasticas',
 }
